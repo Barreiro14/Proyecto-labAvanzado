3a441aa3b5bdd3a80610a59045071d48c8c7c1a9
Gabriel Barreiro <elbarri@omar-pc>
2021-10-21 17:16:39 -0400

    fixed parser

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from solver import solver
 from exact import u_exact
 from visualize import visualize
-from parser import parser
+import argparse
+import numpy as np
+#from parser import parser
 
 def main():
 	#main function
-	parser()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--I', type=float, default=1.0)
+	parser.add_argument('--w', type=float, default=2*np.pi)
+	parser.add_argument('--dt', type=float, default=0.05)
+	parser.add_argument('--num_periods', type=float, default=5)
+	a = parser.parse_args()
+	I, w, dt, num_periods = a.I, a.w, a.dt, a.num_periods
+	#parser()
+	solver(I, w, dt, 10)
 	print("hasta ahora todo jevi")
 
 if __name__ == '__main__':
